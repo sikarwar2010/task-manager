47ab0f074448babdb41437e23b9bec389703a64e
sikarwar2010 <sikarwar2010@gmail.com>
2025-11-02 23:21:52 +0530

    feat:prisma schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Home() {
     return (
-        <div>
-            <h1>Hello world</h1>
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold text-center">Welcome to Ringrlabs</h1>
+
+            <div className="flex items-center justify-center gap-4 mt-6">
+                <Button
+                    variant={"secondary"}
+                    className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    <RegisterLink>Get Started</RegisterLink>
+                </Button>
+
+                <Button asChild variant={"outline"}>
+                    <LoginLink>Login</LoginLink>
+                </Button>
+            </div>
         </div>
     );
 }
